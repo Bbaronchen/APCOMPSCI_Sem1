81b0a2844c323538d74ed3ba770af6ef20016a87
Bbaronchen <baron.chen@sdjaschool.com>
2016-09-06 08:16:07 -0700

    Added Files

---
+++ APLesson_1/ACSII art2.java
@@ -0,0 +1,9 @@
+public class firstProg
+{
+		public static void main(String[]args)
+		{
+				System.out.println(----{,_,">);
+				System.out.println(\,,/(^_^)\,,/);
+				System.out.println(∙،°.  ˘Ô≈ôﺣ   » » »);
+		}
+}
