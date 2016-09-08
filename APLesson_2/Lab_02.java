ab4c64c517fb4841448cb084657ec8fdd1864e74
Bbaronchen <baron.chen@sdjaschool.com>
2016-09-08 08:31:53 -0700

    Add files via upload

---
+++ APLesson_2/Lab_02.java
@@ -0,0 +1,13 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int num1 = 8;
+		int num2 = 6;
+		
+		System.out.println(num1 * num2);
+	}
+}
+
+
+
---
+++ APLesson_2/Lab_02_3.java
@@ -0,0 +1,12 @@
+public class Lab_02_3
+{
+	public static void main(String[]args)
+	{
+		int Length = 13;
+		int Height = 2;
+		int Width = 2;
+		int recSurfaceArea = Length * Width * Height;
+		
+		System.out.println("The surface area of a rectangular cube is" + recSurfaceArea);
+	}
+}
