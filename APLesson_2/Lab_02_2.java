330daf034509afddd1597380c93cf85fc099f2bf
Bbaronchen <baron.chen@sdjaschool.com>
2016-09-08 08:23:24 -0700

    Added Files

---
+++ APLesson_2/Lab_02_2.java
@@ -0,0 +1,12 @@
+public class Lab_02_2
+{
+	public static void main(String[]args)
+	{
+		String first = "4685";
+		String second = "Rancho Pacifica";
+		String third = "San Diego,CA";
+		String fourth = "92130";
+		
+		System.out.println(first + second + third + fourth );
+	}
+}
