0c7f32f1dbbf08cf8d8ce0f6832f752def875641
Bbaronchen <baron.chen@sdjaschool.com>
2016-09-12 09:06:28 -0700

    Add files via upload

--- APLESSON_3/New.java/NEW_01.java
+++ APLESSON_3/New.java/NEW_01.java
@@ -9,24 +9,40 @@ public class NEW_01
 		
 		//prompt for user input
 		System.out.println("What is your name?");
-		System.out.println("How old are you?");
-		System.out.println("What do you do for fun?");
-		System.out.println("What kind of music do you like?");
-		System.out.println("How many siblings do you have?");
-		System.out.println("What do you want to be when you grow up?");
-
+		
 		String name = keyboard.next();
-		String age = keyboard.next();
-		String thing = keyboard.next();
-		String music = keyboard.next();
-		String number = keyboard.next();
-		String job = keyboard.next();
 		
 		System.out.println("Oh" + name + "It's a cool name.");
+		
+		System.out.println("How old are you?");
+		
+	    String age = keyboard.next();
+		
 		System.out.println("Ooooo" + age + "you are so young.");
+		
+		System.out.println("What do you do for fun?");
+		
+		String thing = keyboard.next();
+		
 		System.out.println("Basketball?" + thing + "I like it too.");
+		
+		System.out.println("What kind of music do you like?");
+		
+		String music = keyboard.next();
+		
 		System.out.println("Rock!?" + music + "It's nice.");
+		
+		System.out.println("How many siblings do you have?");
+		
+		String number = keyboard.next();
+		
 		System.out.println("6!?" + number + "That's alot.");
+		
+		System.out.println("What do you want to be when you grow up?");
+
+		String job = keyboard.next();
+		
 		System.out.println("Nice!" + job + "I want to be a teacher too.");
+
 	}
 }
