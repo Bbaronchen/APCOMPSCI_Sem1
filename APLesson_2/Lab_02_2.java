8ec54dadb4477230c43a1c7e280f5e26b88e1777
Chen_Baixin_AP COMPSCIENCE <baron.chen@sdjaschool.com>
2016-09-26 08:11:58 -0700

    Add files via upload

--- APLesson_2/Lab_02_2.java
+++ APLesson_2/Lab_02_2.java
@@ -3,8 +3,8 @@ public class Lab_02_2
 	public static void main(String[]args)
 	{
 		String first = "4685";
-		String second = "Rancho Pacifica";
-		String third = "San Diego,CA";
+		String second = "\tRancho Pacifica";
+		String third = "\tSan Diego,\tCA,";
 		String fourth = "92130";
 		
 		System.out.println(first + second + third + fourth );
