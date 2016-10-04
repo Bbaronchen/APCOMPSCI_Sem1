2a64d8f7df3082736f3a74710048e71114d7eb17
Chen_Baixin_AP COMPSCIENCE <baron.chen@sdjaschool.com>
2016-10-04 07:51:25 -0700

    Add files via upload

--- APLesson_2/Lab_02_2.java
+++ APLesson_2/Lab_02_2.java
@@ -1,12 +1,16 @@
 public class Lab_02_2
 {
-	public static void main(String[]args)
+	public static void main (String[]args)
 	{
-		String first = "4685";
-		String second = "\tRancho Pacifica";
-		String third = "\tSan Diego,\tCA,";
-		String fourth = "92130";
+				
+		String name = "Baron Chen";
+		String address = "1234 Rancho Sierra Bnd";
+		String city = "San Diego";
+		String zip = "92130";
+		System.out.println(name);
+		System.out.println(address);
+		System.out.println(city + "  "  +zip);
 		
-		System.out.println(first + second + third + fourth );
+		
 	}
 }
